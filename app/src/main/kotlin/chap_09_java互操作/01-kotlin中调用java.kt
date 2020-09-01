2e4ff6b29e6ce87cd9de9403c52985395b05f046
wangjiao <1624458215@qq.com>
2020-09-01 18:38:54 +0800

    java与kotlin互调

--- app/src/main/kotlin/chap_09_java互操作/01-kotlin中调用java.kt
+++ app/src/main/kotlin/chap_09_java互操作/01-kotlin中调用java.kt
@@ -1,6 +1,140 @@
 package chap_09_java互操作
 
+import org.jetbrains.annotations.NotNull
+import java.util.*
+import kotlin.collections.ArrayList
+
 /**
  * Created by wangjiao on 2020/8/31.
  * description:
  */
+
+fun demo(source:List<Int>){
+    val list = ArrayList<Int>()
+    for(item in source){
+        list.add(item)
+    }
+    for(i in 0..source.size-1){
+        list[i] = source[i]
+    }
+}
+
+/** 如果java类只有一个setter,它在kotlin中不会作为属性可见，因为kotlin不支持只写（set-only）*/
+fun calendarDemo(){
+    val calendar = Calendar.getInstance()
+    if(calendar.firstDayOfWeek==Calendar.SUNDAY){
+        calendar.firstDayOfWeek = Calendar.MONDAY
+    }
+    if(!calendar.isLenient){
+        calendar.isLenient = true
+    }
+}
+
+/** 将kotlin中是关键字的java标识符进行转义
+ * 如 in ,object ,is 如果一个java库使用了 kotlin关键字作为方法，你仍然可以通过反引号（`）字符转义它来调用该方法
+ * */
+//foo.`is`(bar)
+
+/** 空安全与平台类型
+ * 当调用平台类型变量的方法时，kotlin不会在编译时报告可空性错误，但在运行时调用可能会失败。因为空指针异常或者kotlin生成的阻止空值传播的断言
+ * 如果选中非空类型，编译器会在赋值时触发一个断言，防止kotlin的非空变量保存空值，当我们把平台值传递给期待非空值等的kotlin函数时，也会触发断言。
+ * 总的来说，编译器会尽力阻止空值通过程序向远传播（尽管鉴于泛型的原因，这不可能完全消除）
+ * */
+//fun main(){
+//    val list = ArrayList<String>()
+//    list.add("item")
+//    val size = list.size //非空 （原生 int）
+//    val item = list[1] //推断为平台类型（普通java对象）
+////    item.substring(1)
+//
+//    val nullable:String?=item //允许，没有问题
+//    val notNull:String =item  //允许，运行时可能失败
+//}
+/**平台类型表示法
+ * - T！ 表示“T 或者 T？”
+ * -（Mutable）Collection<T>! 表示“可以可变或不可变、可空或不可空的T的java集合”
+ * - Array<(out)T>! 表示“可空或者不可空的T（或T的子类型）的java数组”
+ * */
+
+/**可空性注解
+ * 具有可空性注解的java类型并不表示为平台类型，而是表示为实际可空或非可空的kotlin类型。编译器支持多种可空性注解，包括：
+ * -JetBrains  （@Nullable @NotNull）
+ * -Android
+ * -JRS-305
+ * -FindBugs
+ * -Eclipse
+ * -Lombok
+ * */
+
+//java声明的注解
+//@NotNull
+//Set<@NotNull String> toSet(@NotNull Collection<@NotNull String> elements){}
+
+//在kotlin中可见的以下签名
+//fun toSet(elements:(Mutable)Collection<String>):(Mutable)Set<String>{}
+
+//如果没有@NotNull注解，类型参数会是平台类型
+//fun toSet(element:(Mutable)Collection<String!>):(Mutable)Set<String!>{}
+
+
+/**类型限定符默认值
+ * ElementType.METHOD 用于方法的返回值
+ * elementType.PARAMETER 用于值参数
+ * elementType.FIELD 用于字段
+ * elemetType.Type_use  用于任意类型
+ * */
+
+/**kotlin中的java泛型
+ * -java中通配符转换成类型投影
+ *  -Foo<? extend Bar>转换成 Foo<out Bar!>!
+ *  -Foo<? super Bar> 转换成 Foo<in Bar!>!
+ *
+ * -java的原始类型转换成星投影
+ *  -list转换成 list<*>! 即 list<out Any?>!
+ * */
+
+/**java反射，可以使用instance::class.java ,ClassName::clas.java  或者instance.javaClass 通过java.lang.Class来进行java反射
+ *
+ * */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/kotlin/chap_09_java互操作/02-java中调用kotLin.kt
+++ app/src/main/kotlin/chap_09_java互操作/02-java中调用kotLin.kt
@@ -1,6 +1,135 @@
 package chap_09_java互操作
 
+import chap_03.Derived
+import java.io.IOException
+
 /**
  * Created by wangjiao on 2020/8/31.
  * description:
  */
+
+/** 多个文件使用相同 java类名，需要使用@JvmMultifileClass注解*/
+////oldutils.kt
+//@file:JvmName("Utils")
+//@file:JvmMultifileClass
+//
+//package org.example
+//fun getTime(){}
+//
+////newutils.kt
+//@file.JvmName("Utils")
+//@file:JvmMultifileClass
+//package org.example
+//fun getDate(){}
+//
+////java
+//org.example.Utils.getTime()
+//org.example.Utils.getDate()
+
+/**实例字段
+ * 当需要在java中将kotlin属性作为字段暴露，则需要使用@JvmField注解。
+ * 如果一个属性有幕后字段，非私有，没有Open/override/const修饰，不是被委托的属性。则可以使用@JvmField
+ * */
+//class User(id:String){
+//    @JvmField val ID=id
+//}
+////java
+//class JavaClient{
+//    public String getID(User user){
+//        return user.ID;
+//    }
+//}
+
+/**静态字段
+ *@JvmStatic
+ * */
+
+/** 接口中的默认方法*/
+
+
+
+/** 用@JvmName解决签名冲突
+ * 有时我们想让一个kotlin中的具名函数在字节码中有另一个JVM名称。最突出的例子是类型擦除引发的
+ * */
+//fun list<String>.filterValid():List<String>
+//fun List<Int>.filterValid():List<Int>
+/** 这两个函数不能同时定义，因为他们JVM签名是一样的。如果真希望他们在kotlin中用相同名称，我们需要用@JvmName去标注其中一个或两个，并
+ * 指定不同名称作为参数
+ * */
+//fun list<String>.filterValid():List<String>
+//
+//@JvmName("filterValidInt")
+//fun List<Int>.filterValid():List<Int>
+/** 在kotlin中可以用相同的名称filterValid来访问，而在java中，它们分别是filterVaild 和 filterValidInt */
+
+/** 同样的技巧也适用于属性X和函数getX()共存*/
+//val x:Int
+//    @JvmName("getC_prop")
+//    get()=15
+//
+//fun getX()=10
+
+/** 如需在没有显示实现getter与setter的情况下更改属性生成的访问器方法的名称，也可以使用@get:JvmName 与 @set:JvmName */
+@get:JvmName("x")
+@set:JvmName("changeX")
+var x:Int =23
+
+
+
+/** 生成重载
+ * @JvmOverloads
+ * 适用于 构造函数，静态函数等
+ * */
+class Circle @JvmOverloads constructor(centerX:Int,centerY:Int,radius:Double=1.0){
+    @JvmOverloads fun draw(label:String,lineWidth:Int=1,color:String="red"){}
+}
+/**对于每一个有默认值的参数，都会生成一个额外的重载，这个重载会把这个参数和它左右的所有参数都移除掉。在上例中，会生成以下代码*/
+//构造函数
+//Circle(int centerX,int centerY,double radius)
+//Circlr(int centerX,int centerY)
+//
+////方法
+//void draw(String label,int lingWidth,String color){}
+//void draw(String label,int lineWidth){}
+//void draw(String label){}
+
+
+/** 受检异常
+ * kotlin没有受检异常，所以，kotlin函数的java签名不会声明抛出异常，假设我们有一个这样的函数
+ * */
+//fun writeToFile(){
+//    throw IOException()
+//}
+//想在java中调用它并捕获这个异常
+//java
+//try{
+//    writeToFile();
+//}catch(IOException e){ //错误，writeToFile()未在throws列表中声明IOException
+//
+//}
+/** 为了解决这个问题，要在kotlin中使用@Throws注解*/
+@Throws(IOException::class)
+fun writeToFile(){
+    throw IOException()
+}
+
+
+
+/** 通配符  @JvmWildcard  和 @JvmSuppressWildcards */
+//fun boxDerived(value:Derived):Box<@JvmWildcard Derived> = Box(value)
+//将被转换成
+//Box<? extends Derived> boxDerived(Derived value){.....}
+
+//fun unboxBase(box:Box<@JvmSuppressWildcards> Base>):Base = box.value
+//会翻译成
+//Base unboxBase(Box<Base> box){....}
+
+
+/** nothing类型*/
+fun emptyList():List<Nothing> = listOf()
+//会被翻译成
+//List emptyList(){}
+
+
+
+
